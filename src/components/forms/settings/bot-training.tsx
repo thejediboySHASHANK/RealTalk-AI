74eabf19a71eb82c902739acb16b67d5a5c622b1
Shashank Dewangan <21052358@kiit.ac.in>
2024-07-11 20:34:10 +0530

    Bot Training Form Initialized

--- src/components/forms/settings/bot-training.tsx
+++ src/components/forms/settings/bot-training.tsx
@@ -4,7 +4,14 @@ type Props = {
 const BotTrainingForm = ({id}: Props) => {
     return (
         <div className="py-5 mb-10 flex flex-col gap-5 items-start">
-
+            <div className="flex flex-col gap-2">
+                <h2 className="font-bold text-2xl">Bot Training</h2>
+                <p className="text-sm font-light">
+                    Set FAQ, questions, create questions for capturing
+                    lead information and
+                    train your bot to act the way you want it to.
+                </p>
+            </div>
         </div>
     );
 };
